05048ab1008850dd9b200002b7478d961639661d
YHesham2510 <yhisham2510@gmail.com>
2025-04-21 02:02:56 +0200

    Youssef | Added discount feature, and fixed all of recent issues + login issue

--- comma-dashboard/src/store/store.js
+++ comma-dashboard/src/store/store.js
@@ -36,7 +36,7 @@ export default createStore({
       commit("setError", null);
 
       try {
-        const response = await fetch("http://localhost:3000/api/login", {
+        const response = await fetch("/api/login", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
